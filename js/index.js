4eaf29a59ae59ba5179746ab85321a66c7d87ec6
Ana Leticya Gomes <120137077+Anagomes9090@users.noreply.github.com>
2026-05-22 22:10:04 -0300

    /

--- js/index.js
+++ js/index.js
@@ -23,3 +23,26 @@ $("#option_1").on("click", function() {
         $('#conteudo_pag_dados_cliente').empty().html(
             '<BR><br><div class="bloco1">'
             )})
+
+////////////////////////////////menu mobile/////////////////////////////////////
+var fundoPgn = document.getElementsByClassName("descarrega_pagina");
+	var abreMenu = document.getElementsByClassName("descarrega");
+	for (let index = 0; index < abreMenu.length; index++) {
+	  abreMenu[index].addEventListener("click", function() {
+		let Menulateral = document.getElementsByClassName("menu-lateral")[0]; 
+		const tamanhoTela = window.innerWidth;  
+		if (tamanhoTela <= 1000) {
+		  // Se a tela for menor ou igual 800px (<= 800px), manipula o menu lateral diretamente com base na visibilidade
+		  if (Menulateral.style.display === "none" || Menulateral.style.display === "") {
+			Menulateral.style.display = "absolute";  // Torna o menu visível
+			Menulateral.style.width = "100%"; // Foi necessário declarar o tamanho do width aqui
+			fundoPgn[0].style.display = "none";  // Esconde o fundo
+	        $('.abre_menu').css('z-index', '0')
+		  } 
+		} else {
+		  // Caso contrário, se a tela for maior que 800px, o menu já está visível
+		  Menulateral.style.display = "absolute";  
+		}
+	 } );
+	break
+}
